76bdae7146468343fb8893a48c6c8ae7e8de1bc0
johannesflommersfeld <johannesflommersfeld@users.noreply.github.com>
2025-05-20 20:55:16 +0200

    fix layout issues with filters dialog

--- src/app/tour-catalog/filters-dialog/filters-dialog.component.ts
+++ src/app/tour-catalog/filters-dialog/filters-dialog.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, Inject, QueryList, ViewChild, ViewChildren, ViewEncapsulation } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialogActions, MatDialogContent } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { RiskLevel } from '../../models/tour-data/risk-level.model';
@@ -9,6 +9,7 @@ import { Aspect } from '../../models/tour-data/aspect.model';
 import { DoubleSliderComponent, DoubleSliderValue } from '../../double-slider/double-slider.component';
 import { Activity } from '../../models/tour-data/activity.model';
 import { AspectPickerComponent } from '../../tour-form/aspect-picker/aspect-picker.component';
+import { Filters } from '../tour-catalog.component';
 
 @Component({
   selector: 'app-filters-dialog',
@@ -17,7 +18,7 @@ import { AspectPickerComponent } from '../../tour-form/aspect-picker/aspect-pick
   styleUrls: ['./filters-dialog.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class FiltersDialogComponent {
+export class FiltersDialogComponent implements AfterViewInit {
   riskLevelType = RiskLevel;
   riskLevelOptions: Options = { floor: RiskLevel.UNKNOWN, ceil: RiskLevel.DANGEROUS, step: 1, showTicks: false };
   riskLevelTranslator!: (value: number) => string;
@@ -41,32 +42,35 @@ export class FiltersDialogComponent {
   travelDurationOptions: Options = { floor: 0, ceil: 24, step: 0.5, showTicks: false };
   travelDurationTranslator!: (value: number) => string;
 
-  filtersForm = new FormGroup({
-    activitiesFlag: new FormControl<number>(Activity.UNDEFINED),
-    distanceRange: new FormControl<DoubleSliderValue<number>>({ lowValue: 0, highValue: 300 }),
-    durationRange: new FormControl<DoubleSliderValue<number>>({ lowValue: 0, highValue: 12 }),
-    metersOfElevationRange: new FormControl<DoubleSliderValue<number>>({ lowValue: 0, highValue: 2000 }),
-    maxMetersOfElevation: new FormControl<number>(Infinity),
-    difficultyRange: new FormControl<DoubleSliderValue<GeneralDifficulty>>({ lowValue: GeneralDifficulty.UNKNOWN, highValue: GeneralDifficulty.VERY_CHALLENGING }),
-    riskRange: new FormControl<DoubleSliderValue<RiskLevel>>({ lowValue: RiskLevel.UNKNOWN, highValue: RiskLevel.DANGEROUS }),
-    travelDistanceRange: new FormControl<DoubleSliderValue<number>>({ lowValue: 0, highValue: 1000 }),
-    travelDurationRange: new FormControl<DoubleSliderValue<number>>({ lowValue: 0, highValue: 24 }),
-    aspects: new FormControl<Aspect>(
-      Aspect.NORTH
-      | Aspect.NORTH_EAST
-      | Aspect.EAST
-      | Aspect.SOUTH_EAST
-      | Aspect.SOUTH
-      | Aspect.SOUTH_WEST
-      | Aspect.WEST
-      | Aspect.NORTH_WEST
-    ),
-  });
+  filtersForm: FormGroup;
+
+  @ViewChildren(DoubleSliderComponent) sliders!: QueryList<DoubleSliderComponent<Number>>;
+
 
   constructor(
     public dialogRef: MatDialogRef<FiltersDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any
-  ) { }
+    @Inject(MAT_DIALOG_DATA) public data: { filters: Filters }) {
+    this.filtersForm = new FormGroup({
+      activitiesFlag: new FormControl<number>(this.data.filters.activitiesFlag ?? Activity.UNDEFINED),
+      distanceRange: new FormControl<DoubleSliderValue<number>>({ lowValue: this.data.filters.minDistance ?? 0, highValue: this.data.filters.maxDistance ?? 300 }),
+      durationRange: new FormControl<DoubleSliderValue<number>>({ lowValue: this.data.filters.minDuration ?? 0, highValue: this.data.filters.maxDuration ?? 12 }),
+      metersOfElevationRange: new FormControl<DoubleSliderValue<number>>({ lowValue: this.data.filters.minMetersOfElevation ?? 0, highValue: this.data.filters.maxMetersOfElevation ?? 2000 }),
+      difficultyRange: new FormControl<DoubleSliderValue<GeneralDifficulty>>({ lowValue: this.data.filters.minDifficulty ?? GeneralDifficulty.UNKNOWN, highValue: this.data.filters.maxDifficulty ?? GeneralDifficulty.VERY_CHALLENGING }),
+      riskRange: new FormControl<DoubleSliderValue<RiskLevel>>({ lowValue: this.data.filters.minRisk ?? RiskLevel.UNKNOWN, highValue: this.data.filters.maxRisk ?? RiskLevel.DANGEROUS }),
+      travelDistanceRange: new FormControl<DoubleSliderValue<number>>({ lowValue: this.data.filters.minTravelDistance ?? 0, highValue: this.data.filters.maxTravelDistance ?? 1000 }),
+      travelDurationRange: new FormControl<DoubleSliderValue<number>>({ lowValue: this.data.filters.minTravelDuration ?? 0, highValue: this.data.filters.maxTravelDuration ?? 24 }),
+      aspects: new FormControl<Aspect>(this.data.filters.aspects ?? (
+        Aspect.NORTH
+        | Aspect.NORTH_EAST
+        | Aspect.EAST
+        | Aspect.SOUTH_EAST
+        | Aspect.SOUTH
+        | Aspect.SOUTH_WEST
+        | Aspect.WEST
+        | Aspect.NORTH_WEST
+      )),
+    });
+  }
 
   ngOnInit(): void {
     this.riskLevelTranslator = (value: number) => {
@@ -84,7 +88,33 @@ export class FiltersDialogComponent {
     };
   }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.sliders.forEach(slider => slider.refreshSlider());
+    }, 0);
+  }
+
   onApply(): void {
-    this.dialogRef.close(false);
+    const formValues = this.filtersForm.value;
+    const filters: Filters = {
+      activitiesFlag: formValues.activitiesFlag,
+      minDistance: formValues.distanceRange.lowValue,
+      maxDistance: formValues.distanceRange.highValue,
+      minDuration: formValues.durationRange.lowValue,
+      maxDuration: formValues.durationRange.highValue,
+      minMetersOfElevation: formValues.metersOfElevationRange.lowValue,
+      maxMetersOfElevation: formValues.metersOfElevationRange.highValue,
+      minDifficulty: formValues.difficultyRange.lowValue,
+      maxDifficulty: formValues.difficultyRange.highValue,
+      minRisk: formValues.riskRange.lowValue,
+      maxRisk: formValues.riskRange.highValue,
+      minTravelDistance: formValues.travelDistanceRange.lowValue,
+      maxTravelDistance: formValues.travelDistanceRange.highValue,
+      minTravelDuration: formValues.travelDurationRange.lowValue,
+      maxTravelDuration: formValues.travelDurationRange.highValue,
+      aspects: formValues.aspects,
+    };
+
+    this.dialogRef.close(filters);
   }
 }
