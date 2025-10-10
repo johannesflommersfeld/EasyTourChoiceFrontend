39e7c37dee53b66079334b14e8ce282e3836efa6
Johannes Flommersfeld <johannes.flommersfeld@tngtech.com>
2025-10-10 17:27:50 +0200

    prepare missing routes

--- src/app/add-tour/add-tour.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-add-tour',
-  imports: [],
-  templateUrl: './add-tour.html',
-  styleUrl: './add-tour.scss'
-})
-export class AddTourComponent {}
---
+++ src/app/tour-form/tour-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tour-form',
+  imports: [],
+  templateUrl: './tour-form.html',
+  styleUrl: './tour-form.scss'
+})
+export class TourFormComponent {}
